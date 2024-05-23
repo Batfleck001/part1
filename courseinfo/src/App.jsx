594d29ee614f3d8b95f32128ac0199b0046a6f87
Batfleck001 <harishsekar1423@gmail.com>
2024-05-23 11:49:26 +0530

    Done 1.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -43,27 +43,29 @@ const App = () => {
   // const part3 = 'State of a  component'
   // const exercises3 = 14
 
-  const course = 'Half Stack application development'
-  const parts = [
-  {
+  const course = {
+    name:'Half Stack application development',
+    parts : [
+    {
     name : "Fundamentals of React",
     exercises:10
-  },
-  {
+    },
+    {
     name : "Using props to pass data",
     exercises:7
-  },
-  {
+    },
+    {
     name : "State of a  component",
     exercises:14
+    }
+    ]
   }
-]
   
   return (
     <div>
-      <Header course = {course} />
-      <Content parts={parts}/>
-      <Footer parts={parts} />
+      <Header course = {course.name} />
+      <Content parts={course.parts}/>
+      <Footer parts={course.parts} />
       </div>
   )
 }
